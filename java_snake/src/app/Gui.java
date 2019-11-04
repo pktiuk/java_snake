72ab5fa9b2b1e06a1c1a7ac15faa19a8e7d936f9
pktiuk <kotiuk@wp.pl>
2019-11-04 22:21:13 +0100

    Create basic Gui class

---
+++ java_snake/src/app/Gui.java
@@ -0,0 +1,70 @@
+package app;
+
+import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.BorderFactory;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class Gui {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    private static void createAndShowGUI() {
+        JFrame f = new JFrame("JavaSnake");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.add(new MyPanel());
+        f.pack();
+        f.setVisible(true);
+    }
+}
+
+class MyPanel extends JPanel implements PropertyChangeListener{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    private int defaultX = 15; // default size of box
+    private int defaultY = 15;
+    private int gridX = 40;
+    private int gridY = 40;
+
+    public MyPanel() {
+
+        setBorder(BorderFactory.createLineBorder(Color.black));
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(defaultX * gridX, defaultY * gridY);
+    }
+    public void propertyChange(PropertyChangeEvent evt) {
+        if(evt.getPropertyName()=="snakeModel")
+        {
+            
+        }
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, defaultX * gridX, defaultY * gridY);
+    }
+
+    protected void drawBox(Graphics g, int x, int y) {
+        g.setColor(Color.WHITE);
+        g.fillRect(x*defaultX, y*defaultY, defaultX, defaultY);
+    }
+}
