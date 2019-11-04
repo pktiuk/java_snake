b036c991f666aeb87d1cbe4c195e5e6d9ad2b174
pktiuk <kotiuk@wp.pl>
2019-11-04 22:49:48 +0100

    Create Model

---
+++ java_snake/src/app/Model.java
@@ -0,0 +1,9 @@
+package app;
+
+/**
+ * <code>Model</code> - model describing state of snake and whole arena,
+ * contains size of arena, state of snake, location of rest of the objects
+ */
+public class Model {
+
+}
