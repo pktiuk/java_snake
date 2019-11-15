0b0f229a1bb1817aaddf33d948fa9668f1485b47
pktiuk <kotiuk@wp.pl>
2019-11-15 13:57:13 +0100

    Add simple class Location containing x and y

---
+++ java_snake/src/app/Location.java
@@ -0,0 +1,15 @@
+package app;
+
+/**
+ * Location
+ */
+public class Location {
+
+    public int x;
+    public int y;
+
+    public Location(int X, int X) {
+        x = X;
+        y = Y;
+    }
+}
