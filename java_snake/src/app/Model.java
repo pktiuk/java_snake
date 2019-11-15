f0b1426d79e0e271cdd4ecd9efad7b5af29c99c3
pktiuk <kotiuk@wp.pl>
2019-11-15 15:56:53 +0100

    Add getters and setters to Model

--- java_snake/src/app/Model.java
+++ java_snake/src/app/Model.java
@@ -2,7 +2,11 @@ package app;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
-import java.util.Vector;;
+import java.util.Vector;
+
+enum Direction {
+    LEFT, RIGHT, UP, DOWN
+}
 
 /**
  * <code>Model</code> - model describing state of snake and whole arena,
@@ -18,9 +22,10 @@ public class Model {
     double tickTime = 0.7;
     // Apple position =(x,y)
     int applePosition[] = new int[2];
-    //Head-beginning
+    // Head-beginning
     Vector<Location> snake = new Vector<Location>();
-    
+    Direction snakeDirection = Direction.UP;
+
     Location apple;
 
     private PropertyChangeSupport pcs = new PropertyChangeSupport(this);
@@ -39,5 +44,52 @@ public class Model {
         for (int i = 0; i < gridX; i++) {
             wall[i] = new boolean[gridY];
         }
+    }
+
+
+
+    public void setTickTime(double newTick) {
+        pcs.firePropertyChange("tickTime", tickTime, newTick);
+        tickTime = newTick;
+    }
+
+    public void setApple(Location newApple) {
+        pcs.firePropertyChange("appleLocation", apple, newApple);
+        apple = newApple;
+    }
+
+    public Location getApple() {
+        return apple;
+    }
+
+    public double getTickTime() {
+        return tickTime;
+    }
+
+    public boolean[][] getFilledTiles() {
+        boolean filledTiles[][] = wall.clone();
+        int a, begin, end;
+        for (int i = 0; i < snake.size() - 1; i++) {
+            if (snake.get(i).x == snake.get(i + 1).x) {
+                a = snake.get(i).x;
+                begin = Math.min(snake.get(i).y, snake.get(i + 1).y);
+                end = Math.max(snake.get(i).y, snake.get(i + 1).y);
+                for (; begin <= end; begin++) {
+                    wall[a][begin] = true;
+                }
+            } else {
+                a = snake.get(i).y;
+                begin = Math.min(snake.get(i).x, snake.get(i + 1).x);
+                end = Math.max(snake.get(i).x, snake.get(i + 1).x);
+                for (; begin <= end; begin++) {
+                    wall[begin][a] = true;
+                }
+            }
+        }
+        return filledTiles;
+    }
+
+    public Location getSnakeHead() {
+        return snake.get(0);
     }
 }
