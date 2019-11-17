9e4089efb5c76a50f2af735348152e4472bcc5e0
pktiuk <kotiuk@wp.pl>
2019-11-17 23:01:57 +0100

    Add constructor to Controller

--- java_snake/src/app/Controller.java
+++ java_snake/src/app/Controller.java
@@ -1,16 +1,18 @@
 package app;
 
+import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.util.Vector;
 
 /**
  * Controller
  */
-/*
-public class Controller implements PropertyChangeListener{
-    private PropertyChangeSupport pcs=new PropertyChangeSupport(this);
+public class Controller implements PropertyChangeListener {
+    private PropertyChangeSupport pcs = new PropertyChangeSupport(this);
     Model m;
     Gui g;
+
     public void addPropertyChangeListener(PropertyChangeListener listener) {
         this.pcs.addPropertyChangeListener(listener);
     }
@@ -19,12 +21,23 @@ public class Controller implements PropertyChangeListener{
         this.pcs.removePropertyChangeListener(listener);
     }
 
-
-    public Controller(int x, int y)
-    {
-        m=Model(x,y);
-        
+    public Controller(int x, int y) {
+        m = new Model(x, y);
+        g = new Gui(x, y);
+        m.addPropertyChangeListener(g.arena);
+        addPropertyChangeListener(g.arena);
+        addPropertyChangeListener(m);
+        g.arena.addPropertyChangeListener(this);
+        Vector<Location> l = new Vector<Location>();
+        l.add(new Location(x / 2, y / 2));
+        l.add(new Location(1 + x / 2, y / 2));
+        m.setSnake(l);
     }
-    
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        // TODO Auto-generated method stub
+
+    }
+
 }
-*/
