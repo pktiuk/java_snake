20711cb0f9132f9207c4cd7b95ce9b9a0454fd1c
pktiuk <kotiuk@wp.pl>
2019-11-17 22:27:45 +0100

    Fix wall initialization, add PropertyChangeSupport and format code

--- java_snake/src/app/Model.java
+++ java_snake/src/app/Model.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.util.Vector;
@@ -12,7 +13,7 @@ enum Direction {
  * <code>Model</code> - model describing state of snake and whole arena,
  * contains size of arena, state of snake, location of rest of the objects
  */
-public class Model {
+public class Model implements PropertyChangeListener {
 
     private int gridX = 40;
     private int gridY = 40;
@@ -41,9 +42,16 @@ public class Model {
     public Model(int x, int y) {
         gridX = x;
         gridY = y;
+        wall = new boolean[x][];
         for (int i = 0; i < gridX; i++) {
             wall[i] = new boolean[gridY];
         }
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        // TODO Auto-generated method stub
+
     }
 
     public void moveSnake(Direction d) {
@@ -66,26 +74,22 @@ public class Model {
                 break;
             }
             snake.insertElementAt(newHead, 0);
-            snakeDirection=d;
+            snakeDirection = d;
         }
-        //distance between 2 last components of snake
-        int dist=snake.lastElement().x+snake.lastElement().y-snake.get(snake.size()-2).x-snake.get(snake.size()-2).y;
-        if(Math.abs(dist)==1)
-            snake.remove(snake.size()-1);
-        else
-        {
-            if(snake.lastElement().x<snake.get(snake.size()-2).x)
-            {
-                snake.lastElement().x+=1;
-            }else if(snake.lastElement().x>snake.get(snake.size()-2).x)
-            {
-                snake.lastElement().x-=1;
-            }else if(snake.lastElement().y<snake.get(snake.size()-2).y)
-            {
-                snake.lastElement().y+=1;
-            }else if(snake.lastElement().y>snake.get(snake.size()-2).y)
-            {
-                snake.lastElement().y-=1;
+        // distance between 2 last components of snake
+        int dist = snake.lastElement().x + snake.lastElement().y - snake.get(snake.size() - 2).x
+                - snake.get(snake.size() - 2).y;
+        if (Math.abs(dist) == 1)
+            snake.remove(snake.size() - 1);
+        else {
+            if (snake.lastElement().x < snake.get(snake.size() - 2).x) {
+                snake.lastElement().x += 1;
+            } else if (snake.lastElement().x > snake.get(snake.size() - 2).x) {
+                snake.lastElement().x -= 1;
+            } else if (snake.lastElement().y < snake.get(snake.size() - 2).y) {
+                snake.lastElement().y += 1;
+            } else if (snake.lastElement().y > snake.get(snake.size() - 2).y) {
+                snake.lastElement().y -= 1;
             }
         }
         pcs.firePropertyChange("snakeModel", null, snake);
