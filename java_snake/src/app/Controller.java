3552e16e56d403c1a4602bbeafd2214a2dd9f584
pktiuk <kotiuk@wp.pl>
2019-11-20 22:10:42 +0100

    Add timer

--- java_snake/src/app/Controller.java
+++ java_snake/src/app/Controller.java
@@ -3,6 +3,8 @@ package app;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.Vector;
 
 /**
@@ -12,6 +14,8 @@ public class Controller implements PropertyChangeListener {
     private PropertyChangeSupport pcs = new PropertyChangeSupport(this);
     Model m;
     Gui g;
+    Direction d = Direction.UP;
+    Timer t = new Timer();
 
     public void addPropertyChangeListener(PropertyChangeListener listener) {
         this.pcs.addPropertyChangeListener(listener);
@@ -22,7 +26,7 @@ public class Controller implements PropertyChangeListener {
     }
 
     public Controller(int x, int y) {
-        m = new Model(x, y);
+        m = new Model(x, y);// Najlepiej w mainie()
         g = new Gui(x, y);
         m.addPropertyChangeListener(g.arena);
         addPropertyChangeListener(g.arena);
@@ -32,6 +36,24 @@ public class Controller implements PropertyChangeListener {
         l.add(new Location(x / 2, y / 2));
         l.add(new Location(1 + x / 2, y / 2));
         m.setSnake(l);
+        t.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                doClockTick();
+            }
+        }, (long) m.getTickTime() * 1000, (long) m.getTickTime() * 1000);
+    }
+
+    void doClockTick() {
+
+        if (m.moveSnakeSucceeded(d)) {
+            // if (m.getApple().equal(m.getSnakeHead())
+
+        } else {
+            pcs.firePropertyChange("gameLost", null, true);
+            t.cancel();
+        }
+
     }
 
     @Override
