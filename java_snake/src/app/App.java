3bf7bdcd884439cedecdd6177c27fbbfb466fb73
pktiuk <kotiuk@wp.pl>
2019-11-22 16:33:10 +0100

    Main file finish

--- java_snake/src/app/App.java
+++ java_snake/src/app/App.java
@@ -1,7 +1,16 @@
 package app;
 
+import javax.swing.SwingUtilities;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Controller c = new Controller(40, 40);
+            }
+        });
     }
+
 }
