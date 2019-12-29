16a9aa21c2f76907b2d0666286a3233073cddb77
pktiuk <kotiuk@wp.pl>
2019-12-29 15:25:06 +0100

    Add generated docs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"app","l":"App"},{"p":"app","l":"Controller"},{"p":"app","l":"Gui"},{"p":"app","l":"Location"},{"p":"app","l":"Model"}]
