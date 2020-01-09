7050b198e7420b9f9e1f642218f4d4bda82cb0d9
pktiuk <kotiuk@wp.pl>
2020-01-09 17:08:09 +0100

    Add scripts  to build and run

---
+++ build.sh
@@ -0,0 +1,2 @@
+javac -d ./java_snake/bin java_snake/src/app/App.java java_snake/src/app/Controller.java  java_snake/src/app/Location.java java_snake/src/app/Model.java java_snake/src/app/Gui.java
+
---
+++ build_and_run.sh
@@ -0,0 +1,3 @@
+sh ./build.sh
+java  -Dfile.encoding=UTF-8 -cp ./java_snake/bin app.App $1 $2
+
